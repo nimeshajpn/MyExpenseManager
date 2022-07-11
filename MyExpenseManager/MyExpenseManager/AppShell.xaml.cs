ad919bc9f34ab1d3c2144a8d107c67a2f4348131
Nadeera Nimesha <Nadeeranimesha456@gmail.com>
2022-07-11 07:57:24 +0530

    Add project files.

---
+++ MyExpenseManager/MyExpenseManager/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using MyExpenseManager.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyExpenseManager
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+           
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ MyExpenseManager/MyExpenseManager/Views/MainExpense.xaml.cs
@@ -0,0 +1,20 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+[assembly: ExportFont("Poppins-Bold.ttf")]
+namespace MyExpenseManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainExpense : ContentPage
+    {
+        public MainExpense()
+        {
+            InitializeComponent();
+        }
+    }
+}
